e5130a43b616b300e94fe2d416b69e247b5728a5
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-28 16:49:08 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "easystr.h"
+
+using namespace std;
+
+int main()
+{   string name;
+    getline (cin,name);
+    cout <<  itc_hello_str(name);
+
+    return 0;
+}
