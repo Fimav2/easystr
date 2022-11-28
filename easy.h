95706825bfd6adc10b9106905d7d86c8d719345f
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-28 16:48:39 +0300

    Create easy.h

---
+++ easy.h
@@ -0,0 +1,12 @@
+#ifndef EASYSTR_H_INCLUDED
+#define EASYSTR_H_INCLUDED
+#include <iostream>
+#include <string>
+
+using namespace std;
+string itc_hello_str(string name);
+
+
+
+
+#endif // EASYSTR_H_INCLUDED
