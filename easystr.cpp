ce6af458edc909e16a49391b60318abbe9a42498
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-01 22:18:08 +0300

    Update easystr.cpp

--- easystr.cpp
+++ easystr.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include "easystr.h"
+#include "easystd.h"
 
 using namespace std;
 
@@ -7,4 +7,13 @@ string itc_hello_str(string name ){
 return ("Hello,"+ name);
 
 }
+string  itc_len(string str){
+    string s;
+while (s){
+    char = c;
+    char [0];
+    c = s / 10;
+    c = c + 1;
+}
 
+}
