8134cc1a4cef8df67996f513f39039cae6537344
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-05 21:24:38 +0300

    Update easystr2.cpp

--- easystr2.cpp
+++ easystr2.cpp
@@ -1,15 +1,4 @@
-int itc_count_char_in_str(char ch, string str){///git
-    int number = 0;
-    for(int i=0; i<itc_len(str); i++){
-            if (ch == str[i]){
-                number = number + 1;
-            }
-        }
 
-
-
-    return number;
-}
 string itc_even_place(string str){
         string g;
         for(int i%2;i<itc_len(str);i++){;
