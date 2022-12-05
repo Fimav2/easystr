6b2595dd6f13ca752898d12baadce6ca7fef00e1
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-05 19:13:13 +0300

    Update easystr.cpp

--- easystr.cpp
+++ easystr.cpp
@@ -22,7 +22,7 @@ long long itc_len(string str){
 // void itc_print_copy_str(string str, int number), которая принимает на вход строку и целое число и
 //выводит на экран строку столько раз, чему равен второй аргумент функции.
 
-void itc_print_copy_str(string str, int number) //Number 3
+void itc_print_copy_str(string str, int number) 
 {
     for (int i = 0; i < number; i++)
         cout << str << " ";
