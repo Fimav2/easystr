707a1c87407b68c58777ca544c7c74a46fd51961
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-05 15:07:34 +0300

    Update easystr.cpp

--- easystr.cpp
+++ easystr.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include "easystd.h"
+#include "easystr.h"
 
 using namespace std;
 
@@ -7,13 +7,23 @@ string itc_hello_str(string name ){
 return ("Hello,"+ name);
 
 }
-string  itc_len(string str){
-    string s;
-while (s){
-    char = c;
-    char [0];
-    c = s / 10;
-    c = c + 1;
-}
+//Написать функцию long long itc_len(string str), которая принимает на вход строку и возвращает на экран целое число количество символов в строке
+long long itc_len(string str){
+    long long b=0;
+    for(int i=0; str[i]!= '\0'; i++){
+        b++;
+    }
+    return b;
+
 
 }
+
+// void itc_print_copy_str(string str, int number), которая принимает на вход строку и целое число и
+//выводит на экран строку столько раз, чему равен второй аргумент функции.
+
+void itc_print_copy_str(string str, int number){
+
+cout<< str[number];
+
+}
+
