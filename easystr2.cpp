857bcee44c1e514b1e60c118fcf1dcad603f6cf5
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-18 21:35:02 +0300

    Update easystr2.cpp

--- easystr2.cpp
+++ easystr2.cpp
@@ -3,16 +3,63 @@
 
 using namespace std;
 string itc_even_place(string str){
-        string g;
-        for(int i%2;i<itc_len(str);i++){;
-      if (str['\0']){
-          return g;
-      }
-        }
-      else{
-      cout<< -1;
-      }
+        if (itc_len(str)>=2){
 
+            string g = "";
+
+            for(int i = 1; i < itc_len(str); i = i + 2){
+                g = g + str[i];
+            }
+
+            return g;
+        }
+
+        else{
+            return "-1";
+        }
+}
+
+
+double itc_percent_lower_uppercase(string str){
+
+ double num_stroch = 0;
+
+ double num_zag = 0;
+
+double percent = 0;
+
+ for (long long i = 0; i <= itc_len(str); ++i){
+
+
+if (str[i] >= 'a' && str[i] <= 'z'){ //-  строчная
+    num_stroch += 1;
+
+}
+
+if (str[i] >= 'A' && str[i] <='Z'){ //- заглавная
+     num_zag +=1;
+}
+
+
+}
+
+percent =  num_zag / num_stroch * 100;
+
+return percent;
+}
+
+
+
+
+
+string itc_reverse_str(string str){
+
+string new_str = "";
+
+for (int i = itc_len(str); i>=0; i--){
+    new_str += str[i];
+}
+return new_str;
 
 }
 
