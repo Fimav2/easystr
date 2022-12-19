b2a58f933caae61ab9bdceafbb8ca8552f145995
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-19 14:56:41 +0300

    Update easystr.h

--- easystr.h
+++ easystr.h
@@ -4,11 +4,15 @@
 #include <string>
 
 using namespace std;
+
 string itc_hello_str(string name);
 
 long long itc_len(string str);
 
 void itc_print_copy_str(string str, int number);
 
+void itc_first_end_three(string str);
+
+int itc_count_char_in_str(char ch, string str);
 
 #endif // EASYSTR_H_INCLUDED
