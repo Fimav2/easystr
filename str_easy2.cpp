e70948efba6c74b837da47ef811798ac574653a6
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-21 16:44:23 +0300

    Update str_easy2.cpp

--- str_easy2.cpp
+++ str_easy2.cpp
@@ -56,7 +56,7 @@ string itc_reverse_str(string str){
 
 string new_str = "";
 
-for (int i = itc_len(str); i>=0; i--){
+for (int i = itc_len(str) -1; i>=0; i--){
     new_str += str[i];
 }
 return new_str;
