df2e866df98502835ce9a1d24be09bdd663cb64f
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-25 12:41:40 +0300

    Update str_easy3.cpp

--- str_easy3.cpp
+++ str_easy3.cpp
@@ -1,1 +1,39 @@
+#include <string>
+#include "str_easy.h"
 
+using namespace std;
+string itc_cmp_str(string str1, string str2, int num){
+
+    string g;
+
+    int i, j, h;
+
+    h = itc_len(str1);
+
+    if (str2[0] == '\0')
+
+        return str1;
+
+    for (i = 0; i < num; i++)
+
+        g = g + str1[i];
+
+    for (j = 0; str2[j] != '\0'; j++)
+
+        if (i < h)
+        {
+           g = g + str2[j];
+
+        i++;
+
+        }
+
+    if (i <= h)
+
+        for (i; str1[i] !='\0'; i++)
+
+            g = g + str1[i];
+
+
+    return g;
+}
