fc354a4130b999d8846af2637e65c9a00906b34e
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-12-26 14:04:35 +0300

    Update str_easy2.cpp

--- str_easy2.cpp
+++ str_easy2.cpp
@@ -2,6 +2,7 @@
 #include "str_easy.h"
 
 using namespace std;
+
 string itc_even_place(string str){
         if (itc_len(str)>=2){
 
@@ -18,7 +19,6 @@ string itc_even_place(string str){
             return "-1";
         }
 }
-
 
 double itc_percent_lower_uppercase(string str){
 
@@ -47,48 +47,6 @@ percent =  num_zag / num_stroch * 100;
 
 return percent;
 }
-string itc_slice_str(string str,int tank, int nazad)
-{
-    string new_str;
-
-    int i;
-
-    if (tank < nazad)
-    {
-         while (i != tank)
-        i = i + 1;
-
-    for (i; i <= nazad; i++)
-
-         new_str = new_str + str[i];
-
-    }
-    else
-
-        new_str = "0";
-
-    return new_str;
-}
- bool itc_equal_reverse(string str){
-
-     string g;
-
-     g = itc_reverse_str(str);
-
-     if (g == str){
-
-            return 1;
-     }
-
-        else {
-                return 0;
-        }
-
- }
-
-
-
-
 string itc_reverse_str(string str){
 
 string new_str = "";
@@ -99,6 +57,7 @@ for (int i = itc_len(str) -1; i>=0; i--){
 return new_str;
 
 }
+
 string itc_slice_str(string str,int tank, int nazad){
 
     string new_str;
@@ -121,7 +80,7 @@ string itc_slice_str(string str,int tank, int nazad){
 
     return new_str;
 }
- bool itc_equal_reverse(string str){
+bool itc_equal_reverse(string str){
 
      string g;
 
@@ -137,4 +96,3 @@ string itc_slice_str(string str,int tank, int nazad){
         }
 
  }
- 
